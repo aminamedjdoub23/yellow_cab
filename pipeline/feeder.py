61deb877ff5d9959f4b63685c197f0ba225305f2
Nouveau Nom Utilisateur <amina.medjdoub2@gmail.com>
2026-03-24 20:55:32 +0100

    ajout des scripts pipeline  (feeder, processorr)

---
+++ pipeline/feeder.py
@@ -0,0 +1,66 @@
+from datetime import date
+from pyspark.sql import SparkSession, functions as F
+import time
+import sys
+
+log_file = open("feeder.txt", "w")
+sys.stdout = log_file
+log_file.write("Feeder started at {}".format(time.time()))
+
+spark = (
+    SparkSession.builder
+    .appName("feeder")
+    .getOrCreate()
+)
+
+
+input_path_trips = "file:///source/nyc_yellow_taxi_dataset/yellow_tripdata_*_snappy.parquet" 
+df_trips = (
+    spark.read
+    .parquet(input_path_trips)
+)
+
+input_path_zone = "file:///source/nyc_yellow_taxi_dataset/taxi_zone_lookup.csv" 
+df_zone = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(input_path_zone)
+)
+
+today = date.today()
+df_trips_partitioned = (
+    df_trips.withColumn("year", F.year(F.col("tpep_pickup_datetime")))
+            .withColumn("month", F.month(F.col("tpep_pickup_datetime")))
+            .withColumn("day", F.dayofmonth(F.col("tpep_pickup_datetime")))
+)
+
+df_trips_partitioned.persist()
+
+df_trips_partitioned.show(10)
+
+r =  df_trips_partitioned.count()
+print("Nombre de lignes : {}".format(r))
+
+output_base_trips = "hdfs://namenode:9000/raw/trips"
+output_base_zones = "hdfs://namenode:9000/raw/zones"
+
+time.sleep(120)
+
+(
+    df_trips_partitioned.repartition(4)
+    .write
+    .mode("overwrite")
+    .partitionBy("year", "month", "day")
+    .parquet(output_base_trips)
+)
+
+(
+    df_zone.write
+    .mode("overwrite")
+    .parquet(output_base_zones)
+)
+
+log_file.write("Feeder finished at {}".format(time.time()))
+spark.stop()
+log_file.close()
---
+++ pipeline/processor.py
@@ -0,0 +1,90 @@
+from datetime import date
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.window import Window
+import time
+import sys
+
+# log txt
+log_file = open("processor.txt", "w")
+sys.stdout = log_file
+log_file.write("Processor started at {}".format(time.time()))
+
+# init avec hive support
+spark = (
+    SparkSession.builder
+    .appName("processor")
+    .enableHiveSupport()
+    .getOrCreate()
+)
+
+trips_path = "hdfs://namenode:9000/raw/trips"
+zones_path = "hdfs://namenode:9000/raw/zones"
+df_trips = spark.read.parquet(trips_path)
+df_zones = spark.read.parquet(zones_path)
+
+# nettoyage base
+df_trips = df_trips.filter(F.col("fare_amount") > 0)
+df_trips = df_trips.filter(F.col("trip_distance") > 0)
+df_trips = df_trips.filter(F.col("tpep_dropoff_datetime") > F.col("tpep_pickup_datetime")) 
+df_trips = df_trips.filter((F.col("passenger_count") >= 1) & (F.col("passenger_count") <= 6))
+
+# recup ids valides pr eviter les erreurs de zones
+zones_valides = [row.LocationID for row in df_zones.select("LocationID").distinct().collect()]
+df_trips = df_trips.filter(
+    F.col("PULocationID").isin(zones_valides) & 
+    F.col("DOLocationID").isin(zones_valides)
+)
+
+# renommage
+pickup_zones = df_zones.withColumnRenamed("LocationID", "PU_LocID") \
+                       .withColumnRenamed("Borough", "pickup_borough") \
+                       .withColumnRenamed("Zone", "pickup_zone")
+
+dropoff_zones = df_zones.withColumnRenamed("LocationID", "DO_LocID") \
+                        .withColumnRenamed("Borough", "dropoff_borough") \
+                        .withColumnRenamed("Zone", "dropoff_zone")
+
+# joins
+df_joined = df_trips.join(pickup_zones, df_trips.PULocationID == pickup_zones.PU_LocID, "inner")
+df_joined = df_joined.join(dropoff_zones, df_joined.DOLocationID == dropoff_zones.DO_LocID, "inner")
+
+# dico de payment_type direct
+payment_data = [
+    (1, "Credit card"),
+    (2, "Cash"),
+    (3, "No charge"),
+    (4, "Dispute"),
+    (5, "Unknown"),
+    (6, "Voided trip")
+]
+df_payment_lookup = spark.createDataFrame(payment_data, ["payment_id", "payment_method"])
+df_joined = df_joined.join(df_payment_lookup, df_joined.payment_type == df_payment_lookup.payment_id, "inner")
+
+df_joined.persist()
+
+df_joined = df_joined.withColumn("pickup_hour", F.hour(F.col("tpep_pickup_datetime")))
+df_joined = df_joined.withColumn("pickup_date", F.to_date(F.col("tpep_pickup_datetime")))
+
+# agg_1 revenue
+df_revenue = df_joined.groupBy("pickup_zone", "pickup_hour") \
+                      .agg(F.avg("fare_amount").alias("revenu_moyen"))
+
+# fct window + rank 
+window_spec = Window.partitionBy("pickup_zone").orderBy(F.col("revenu_moyen").desc())
+df_revenue = df_revenue.withColumn("rang_rentabilite", F.rank().over(window_spec))
+
+# agg_2 courses / jour
+df_courses = df_joined.groupBy("pickup_date", "pickup_borough") \
+                      .agg(F.count("*").alias("nb_courses"))
+
+
+# hive
+spark.sql("CREATE DATABASE IF NOT EXISTS silver")
+
+df_joined.write.mode("overwrite").format("parquet").saveAsTable("silver.trips_cleaned")
+df_revenue.write.mode("overwrite").format("parquet").saveAsTable("silver.revenue_par_zone_heure")
+df_courses.write.mode("overwrite").format("parquet").saveAsTable("silver.courses_par_jour_borough")
+
+spark.stop()
+log_file.write("\nProcessor finished at {}".format(time.time()))
+log_file.close()
